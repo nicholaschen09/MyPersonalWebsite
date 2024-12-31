0001c609be47693fd7695c914a2f9cb38a11c80c
nicholaschen09 <nicholas.chen243@gmail.com>
2024-12-31 16:31:29 -0500

    add more pages

---
+++ .history/src/components/Solidworks_20241231162820.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SolidWorks.css';
+
+const SolidWorks = () => {
+    return (
+        <div className="solidworks">
+            <h1>SolidWorks Project</h1>
+            <p>This is a detailed description of my SolidWorks project.</p>
+            <p>Here you can add more information about the project, images, videos, etc.</p>
+        </div>
+    );
+};
+
+export default SolidWorks;
---
+++ .history/src/components/Solidworks_20241231163053.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Solidworks.css';
+
+const Solidworks = () => {
+    return (
+        <div className="solidworks">
+            <h1>portfolio</h1>
+            <p>This is a detailed description of my SolidWorks project.</p>
+            <p>Here you can add more information about the project, images, videos, etc.</p>
+        </div>
+    );
+};
+
+export default Solidworks;
